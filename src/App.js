f23f2ce6a199945cc116805ccfc75a34f871ee72
Giltroy Meren <giltroymeren@gmail.com>
2020-07-08 11:55:42 +0800

    Using prev & next from data

--- src/App.js
+++ src/App.js
@@ -27,82 +27,9 @@ const Starship = ({ name, crew, passengers, hyperdrive}) => {
   );
 }
 
-const data = [
-  {
-    "name": "CR90 corvette",
-    "model": "CR90 corvette",
-    "manufacturer": "Corellian Engineering Corporation",
-    "cost_in_credits": "3500000",
-    "length": "150",
-    "max_atmosphering_speed": "950",
-    "crew": "30-165",
-    "passengers": "600",
-    "cargo_capacity": "3000000",
-    "consumables": "1 year",
-    "hyperdrive_rating": "2.0",
-    "MGLT": "60",
-    "starship_class": "corvette",
-    "pilots": [],
-    "films": [
-        "http://swapi.dev/api/films/1/",
-        "http://swapi.dev/api/films/3/",
-        "http://swapi.dev/api/films/6/"
-    ],
-    "created": "2014-12-10T14:20:33.369000Z",
-    "edited": "2014-12-20T21:23:49.867000Z",
-    "url": "http://swapi.dev/api/starships/2/"
-},
-{
-    "name": "Star Destroyer",
-    "model": "Imperial I-class Star Destroyer",
-    "manufacturer": "Kuat Drive Yards",
-    "cost_in_credits": "150000000",
-    "length": "1,600",
-    "max_atmosphering_speed": "975",
-    "crew": "47,060",
-    "passengers": "n/a",
-    "cargo_capacity": "36000000",
-    "consumables": "2 years",
-    "hyperdrive_rating": "2.0",
-    "MGLT": "60",
-    "starship_class": "Star Destroyer",
-    "pilots": [],
-    "films": [
-        "http://swapi.dev/api/films/1/",
-        "http://swapi.dev/api/films/2/",
-        "http://swapi.dev/api/films/3/"
-    ],
-    "created": "2014-12-10T15:08:19.848000Z",
-    "edited": "2014-12-20T21:23:49.870000Z",
-    "url": "http://swapi.dev/api/starships/3/"
-},
-{
-    "name": "Sentinel-class landing craft",
-    "model": "Sentinel-class landing craft",
-    "manufacturer": "Sienar Fleet Systems, Cyngus Spaceworks",
-    "cost_in_credits": "240000",
-    "length": "38",
-    "max_atmosphering_speed": "1000",
-    "crew": "5",
-    "passengers": "75",
-    "cargo_capacity": "180000",
-    "consumables": "1 month",
-    "hyperdrive_rating": "1.0",
-    "MGLT": "70",
-    "starship_class": "landing craft",
-    "pilots": [],
-    "films": [
-        "http://swapi.dev/api/films/1/"
-    ],
-    "created": "2014-12-10T15:48:00.586000Z",
-    "edited": "2014-12-20T21:23:49.873000Z",
-    "url": "http://swapi.dev/api/starships/5/"
-}
-];
-
 const App = () => {
   // TODO: show API page 1 on load
-  const [ starships, setStarships ] = useState(data);
+  const [ starships, setStarships ] = useState([]);
   const [ currentPage, setCurrentPage ] = useState(1);
   const [ prevButtonState, setPrevButtonState ] = useState('');
   const [ nextButtonState, setNextButtonState ] = useState('');
@@ -110,6 +37,19 @@ const App = () => {
   const handlePageClick = (action) => {
     console.log('action');
   }
+
+  useEffect(() => {
+    console.log(`CALLED: https://swapi.dev/api/starships/?page=${currentPage}`);
+    axios.get(`https://swapi.dev/api/starships/?page=${currentPage}`)
+      .then(response => {
+        const data = response.data;
+        console.log(data);
+
+        setStarships(data.results);
+        setPrevButtonState(data.previous === null ? 'disabled' : '');
+        setNextButtonState(data.next ? '' : 'disabled');
+      });
+  });
 
   return (
     <div className="App">
